2f5b96c5768d32ff4e443bb89d5a60df2e1e0f93
AdvaithD <advaith@chainist.com>
2021-12-24 04:23:04 -0800

    get 100k uniswap pairs - 12s over ws

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module chainrunner
 go 1.15
 
 require (
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/ethereum/go-ethereum v1.10.14
 	github.com/joho/godotenv v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"chainrunner/services"
+	"chainrunner/uniquery"
+	"chainrunner/util"
+	"fmt"
+	"log"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 )
 
 // get uniswap pairs to bootstrap reserves data
-func getUniswapPairs() {
-
+func getUniswapPairs(query *uniquery.FlashBotsUniswapQuery) {
+	defer util.Duration(util.Track("getUniswapPairs - 100k"))
+	factory := common.HexToAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")
+	pairs, err := query.GetPairsByIndexRange(&bind.CallOpts{}, factory, big.NewInt(0), big.NewInt(100000))
+	if err != nil {
+		log.Fatalf("err getting data")
+	}
+	fmt.Println("Got pairs", len(pairs), pairs)
 }
 
 
@@ -19,5 +32,24 @@ func main() {
 	// string client = flag.String("client", "xxx", "Gateway to the bsc protocol. Available options:\n\t-bsc_testnet\n\t-bsc\n\t-geth_http\n\t-geth_ipc")
 
 	// create client
-	rpcClient := services.InitRPCClient()
+	// rpcClient := services.InitRPCClient()
+	conn, err := ethclient.Dial("ws://157.90.35.22:8545")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	// address where multicall contract is located
+	uniqueryAddr := common.HexToAddress("0x5EF1009b9FCD4fec3094a5564047e190D72Bd511")
+	// uniswap factory address
+	// uniFactoryAddr := "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+
+	// query the contract
+	uniquery, err := uniquery.NewFlashBotsUniswapQuery(uniqueryAddr, conn)
+
+	if err != nil {
+		fmt.Println("error initiating contract to query mass")
+	}
+
+	getUniswapPairs(uniquery)
 }
