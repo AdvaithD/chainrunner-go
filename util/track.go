a4e571684c3b6be0d3ec057bd68560a15c21b186
AdvaithD <advaith@chainist.com>
2021-12-24 03:53:50 -0800

    add: leveldb, util tracker

---
+++ util/track.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"log"
+	"time"
+)
+
+// Track function execution time
+// e.g: defer util.Duration(util.Track("foo"))
+// the above goes at top of function
+
+
+func Track(msg string) (string, time.Time) {
+    return msg, time.Now()
+}
+
+func Duration(msg string, start time.Time) {
+    log.Printf("%v: %v\n", msg, time.Since(start))
+}
