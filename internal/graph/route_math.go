d3b5d4fc7e4f2da6de7d408dc200ac160ffc4112
AdvaithD <advaith@chainist.com>
2022-02-26 02:29:05 +0100

    route math file that wraps around getamount* calls

--- internal/graph/route_math.go
+++ internal/graph/route_math.go
@@ -1,7 +1,12 @@
 package graph
 
+import (
+	"chainrunner/internal/global"
+
+	"github.com/ethereum/go-ethereum/common"
+)
 
 // Naive calulationg over a route given reserves
-func EvaluatePath(route []int, reserves map[common.Address]*globa.PoolReserve) {
+func EvaluatePath(route []int, reserves map[common.Address]*global.PoolReserve) {
 
 }
